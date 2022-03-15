9efaeb7e06e85d6ccf6637d6cb608e3a8386de18
isaac4k <isaactigistu184@gmail.com>
2022-03-15 09:35:24 -0900

    vbgm

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign(int n) - check the code.
+ * @n: an integer
+ * Return:1 if @n is positive number, 0 for zero
+ * and -1 for negative number*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+		else
+		{
+			_putchar('-');
+			return (-1);
+		}
+	}  
+	
