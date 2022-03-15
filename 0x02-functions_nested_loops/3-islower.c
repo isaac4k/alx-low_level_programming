7458f31a342bddec040bc1840222fde21f4e41ec
isaac4k <isaactigistu184@gmail.com>
2022-03-15 08:54:55 -0900

    hello

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  * Description - check if the given char is lower case or not
  * Return: 1 if the input is lower else 0
  */
-int _islowe(int c)
+int _islower(int c)
 {
     if (c >= 97 && c <= 122)
     {
