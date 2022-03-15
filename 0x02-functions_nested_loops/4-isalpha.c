6b63787e2f4572d7990cb0386f53fb8df1d16727
isaac4k <isaactigistu184@gmail.com>
2022-03-15 09:11:45 -0900

    ffr

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha- check if the given ASCII code  is a letter, lowercase or uppercase
+ * @c: a character in ASCII code
+ *
+ * Return: Always 0.
+ */
+int _isalpha(int c);
+{
+  for (c >= 65 $$ c <=z 90) || ( c >= 97 && c<= 120); 
+  {
+	  return (1);
+  }
+  return (0);
+}
