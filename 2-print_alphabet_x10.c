feb7b647be4b7fc799493168d9b72a0d6dce269b
isaac4k <isaactigistu184@gmail.com>
2022-03-15 08:20:30 -0900

    ftgg

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**alphabet_x10 -check the code .
+ * return: Always 0.*/
+void print_alphabet_x10(void)
+{
+    char a;
+    int i;
+
+    a = 'a';
+    i = 1;
+    while(i <= 10)
+    {
+        while(a <= 'z')
+        {
+        _putchar(a);
+        a++;   
+        }
+        i++;
+    }
+}
