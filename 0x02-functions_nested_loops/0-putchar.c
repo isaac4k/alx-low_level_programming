b29f8cbe2912091a1edd54766bf1710cc23cd6b9
isaac4k <isaactigistu184@gmail.com>
2022-03-15 07:30:28 -0700

    ddd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ * Description - print putchar
+ * Return: Always 0 (success)
+ */
+
+int main()
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+     return 0;
+}
