9300207a73387ad3223839070f4150e5f1cb3cf5
isaac4k <isaactigistu184@gmail.com>
2022-04-26 23:35:08 -0900

    drfg

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * _check_and_free - free a list
+ * @head: of the list
+ * @prev: prev node in the list
+ *
+ * Return: number of nodes
+ */
+int _check_and_free(listint_t *head, listint_safe *prev)
+{
+	listint_safe node, *tmp;
+	int count;
+
+	if (head->next == NULL)
+	{
+		free(head);
+		return (1);
+	}
+
+	node.next = prev;
+	node.addy = head;
+	tmp = node.next;
+	while (tmp != NULL && tmp->addy != head)
+		tmp = tmp->next;
+	if (tmp != NULL)
+		return (0);
+
+	count = 1;
+	count += _check_and_free(head->next, &node);
+	free(head);
+	return (count);
+}
+/**
+ * free_listint_safe - free a listint_t safely
+ * @h: head of list
+ *
+ * Return: size of list free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	int t;
+
+	if (*h == NULL)
+		return (0);
+
+	t = _check_and_free(*h, NULL);
+	*h = NULL;
+	return (t);
+}
---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * _check_and_find - Check the list and return the looping node
+ * @head: of list to check
+ * @prev: prev node on the list
+ *
+ * Return: Node that loops, or NULL
+ */
+listint_t *_check_and_find(listint_t *head, listint_safe *prev)
+{
+	listint_safe node, *tmp;
+
+	if (head->next == NULL)
+		return (NULL);
+
+	node.next = prev;
+	node.addy = head;
+	tmp = node.next;
+	while (tmp != NULL && tmp->addy != head)
+		tmp = tmp->next;
+	if (tmp != NULL)
+		return (head);
+
+	return (_check_and_find(head->next, &node));
+}
+/**
+ * find_listint_loop - find if list
+ * @head: of the list
+ *
+ * Return: Node that loops, or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL)
+		return (NULL);
+
+	return (_check_and_find(head, NULL));
+}
